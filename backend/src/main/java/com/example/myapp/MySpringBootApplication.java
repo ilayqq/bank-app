d31cab04551522150742183eff9bbd13d7585fbd
ilayqq <77678193+ilayqq@users.noreply.github.com>
2025-04-25 09:04:38 +0500

    add jwt to swagger

--- backend/src/main/java/com/example/myapp/MySpringBootApplication.java
+++ backend/src/main/java/com/example/myapp/MySpringBootApplication.java
@@ -1,9 +1,27 @@
 package com.example.myapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = ""),
+        security = @SecurityRequirement(name = "Bearer Authentication")
+)
+@SecuritySchemes({
+        @SecurityScheme(
+                name = "Bearer Authentication",
+                type = SecuritySchemeType.HTTP,
+                scheme = "bearer",
+                bearerFormat = "JWT"
+        )
+})
 public class MySpringBootApplication {
 
     public static void main(String[] args) {
